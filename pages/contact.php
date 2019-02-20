74fad5723980cb98212b8924e1cb3b1fa2097166
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 16:40:16 +0100

    modif contact html + style

--- pages/contact.php
+++ pages/contact.php
@@ -2,118 +2,157 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="UTF-8" />
-    <link  href="./pages/style.contact.pages.css" rel="stylesheet"/>
+
 </head>
 
-<form>
-    <ul class="form-style-1">
-        <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" /> <input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
-        <li>
-            <label>Email <span class="required">*</span></label>
-            <input type="email" name="field3" class="field-long" />
-        </li>
-        <li>
-            <label>Subject</label>
-            <select name="field4" class="field-select">
-                <option value="Advertise">Advertise</option>
-                <option value="Partnership">Partnership</option>
-                <option value="General Question">General</option>
-            </select>
-        </li>
-        <li>
-            <label>Your Message <span class="required">*</span></label>
-            <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
-        </li>
-        <li>
-            <input type="submit" value="Submit" />
-        </li>
-    </ul>
-</form>
+<div class="form-style-10">
+    <h1> Inscrivez-vous maintenant!<span> Inscrivez-vous et dites nous ce que vous pensez!</span></h1>
+    <form>
+        <div class="section"><span>1</span> Nom & Adresse</div>
+        <div class="inner-wrap">
+            <label>Votre nom complet <input type="text" name="field1" /></label>
+            <label>Address <textarea name="field2"></textarea></label>
+        </div>
 
+        <div class="section"><span>2</span>Email & Phone</div>
+        <div class="inner-wrap">
+            <label>Email  <input type="email" name="field3" /></label>
+            <label> Numéro de mobile <input type="text" name="field4" /></label>
+        </div>
+
+        <div class="section"><span>3</span>Passwords</div>
+        <div class="inner-wrap">
+            <label> Mot de passe <input type="password" name="field5" /></label>
+            <label>Confirmer votre mot de passe  <input type="password" name="field6" /></label>
+        </div>
+        <div class="button-section">
+            <input type="submit" name="Sign Up" />
+            <span class="privacy-policy">
+     <input type="checkbox" name="field7"./> politique de confidentialité.
+     </span>
+        </div>
+    </form>
+</div>
+
+<link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
 <style type="text/css">
-    .form-style-1 {
-        margin:10px auto;
-        max-width: 400px;
-        padding: 20px 12px 10px 20px;
-        font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    .form-style-10{
+        width:450px;
+        padding:30px;
+        margin:40px auto;
+        background: #FFF;
+        border-radius: 10px;
+        -webkit-border-radius:10px;
+        -moz-border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+        -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+        -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
     }
-    .form-style-1 li {
-        padding: 0;
+    .form-style-10 .inner-wrap{
+        padding: 30px;
+        background: #F8F8F8;
+        border-radius: 6px;
+        margin-bottom: 15px;
+    }
+    .form-style-10 h1{
+        background: #2A88AD;
+        padding: 20px 30px 15px 30px;
+        margin: -30px -30px 30px -30px;
+        border-radius: 10px 10px 0 0;
+        -webkit-border-radius: 10px 10px 0 0;
+        -moz-border-radius: 10px 10px 0 0;
+        color: #fff;
+        text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+        font: normal 30px 'Bitter', serif;
+        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        border: 1px solid #257C9E;
+    }
+    .form-style-10 h1 > span{
         display: block;
-        list-style: none;
-        margin: 10px 0 0 0;
+        margin-top: 2px;
+        font: 13px Arial, Helvetica, sans-serif;
     }
-    .form-style-1 label{
-        margin:0 0 3px 0;
-        padding:0px;
-        display:block;
-        font-weight: bold;
+    .form-style-10 label{
+        display: block;
+        font: 13px Arial, Helvetica, sans-serif;
+        color: #888;
+        margin-bottom: 15px;
     }
-    .form-style-1 input[type=text],
-    .form-style-1 input[type=date],
-    .form-style-1 input[type=datetime],
-    .form-style-1 input[type=number],
-    .form-style-1 input[type=search],
-    .form-style-1 input[type=time],
-    .form-style-1 input[type=url],
-    .form-style-1 input[type=email],
-    textarea,
-    select{
+    .form-style-10 input[type="text"],
+    .form-style-10 input[type="date"],
+    .form-style-10 input[type="datetime"],
+    .form-style-10 input[type="email"],
+    .form-style-10 input[type="number"],
+    .form-style-10 input[type="search"],
+    .form-style-10 input[type="time"],
+    .form-style-10 input[type="url"],
+    .form-style-10 input[type="password"],
+    .form-style-10 textarea,
+    .form-style-10 select {
+        display: block;
         box-sizing: border-box;
         -webkit-box-sizing: border-box;
         -moz-box-sizing: border-box;
-        border:1px solid #BEBEBE;
-        padding: 7px;
-        margin:0px;
-        -webkit-transition: all 0.30s ease-in-out;
-        -moz-transition: all 0.30s ease-in-out;
-        -ms-transition: all 0.30s ease-in-out;
-        -o-transition: all 0.30s ease-in-out;
-        outline: none;
-    }
-    .form-style-1 input[type=text]:focus,
-    .form-style-1 input[type=date]:focus,
-    .form-style-1 input[type=datetime]:focus,
-    .form-style-1 input[type=number]:focus,
-    .form-style-1 input[type=search]:focus,
-    .form-style-1 input[type=time]:focus,
-    .form-style-1 input[type=url]:focus,
-    .form-style-1 input[type=email]:focus,
-    .form-style-1 textarea:focus,
-    .form-style-1 select:focus{
-        -moz-box-shadow: 0 0 8px #88D5E9;
-        -webkit-box-shadow: 0 0 8px #88D5E9;
-        box-shadow: 0 0 8px #88D5E9;
-        border: 1px solid #88D5E9;
-    }
-    .form-style-1 .field-divided{
-        width: 49%;
+        width: 100%;
+        padding: 8px;
+        border-radius: 6px;
+        -webkit-border-radius:6px;
+        -moz-border-radius:6px;
+        border: 2px solid #fff;
+        box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+        -moz-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+        -webkit-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
     }
 
-    .form-style-1 .field-long{
-        width: 100%;
+    .form-style-10 .section{
+        font: normal 20px 'Bitter', serif;
+        color: #2A88AD;
+        margin-bottom: 5px;
     }
-    .form-style-1 .field-select{
-        width: 100%;
-    }
-    .form-style-1 .field-textarea{
-        height: 100px;
-    }
-    .form-style-1 input[type=submit], .form-style-1 input[type=button]{
-        background: #4B99AD;
-        padding: 8px 15px 8px 15px;
-        border: none;
+    .form-style-10 .section span {
+        background: #2A88AD;
+        padding: 5px 10px 5px 10px;
+        position: absolute;
+        border-radius: 50%;
+        -webkit-border-radius: 50%;
+        -moz-border-radius: 50%;
+        border: 4px solid #fff;
+        font-size: 14px;
+        margin-left: -45px;
         color: #fff;
+        margin-top: -3px;
     }
-    .form-style-1 input[type=submit]:hover, .form-style-1 input[type=button]:hover{
-        background: #4691A4;
-        box-shadow:none;
-        -moz-box-shadow:none;
-        -webkit-box-shadow:none;
+    .form-style-10 input[type="button"],
+    .form-style-10 input[type="submit"]{
+        background: #2A88AD;
+        padding: 8px 20px 8px 20px;
+        border-radius: 5px;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        color: #fff;
+        text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+        font: normal 30px 'Bitter', serif;
+        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+        border: 1px solid #257C9E;
+        font-size: 15px;
     }
-    .form-style-1 .required{
-        color:red;
+    .form-style-10 input[type="button"]:hover,
+    .form-style-10 input[type="submit"]:hover{
+        background: #2A6881;
+        -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+        -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+        box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+    }
+    .form-style-10 .privacy-policy{
+        float: right;
+        width: 250px;
+        font: 12px Arial, Helvetica, sans-serif;
+        color: #4D4D4D;
+        margin-top: 10px;
+        text-align: right;
     }
 </style>
-
-</html>
