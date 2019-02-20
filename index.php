40789274e98f4f3953ed8b8fe56c3868415634e6
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 10:43:02 +0100

    index mis a jour

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Salomé
- * Date: 20/02/2019
- * Time: 10:28
- */
+session_start();
+/*if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+    echo "Vous etes connecte";
+}
+else {
+    echo "Vous n'etes pas connecte";
+}*/
+//echo session_id();
+date_default_timezone_set('Europe/Paris');
+include_once "./includes/html.php";
+include_once "./includes/header.php";
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+else {
+    $page = "accueil";
+}
+$path = "./includes/";
+$contenu = glob($path . "*.inc.php");
+$page = $path . $page . ".inc.php";
+if (in_array($page, $contenu)) {
+    include_once $page;
+}
+else {
+    include_once "./includes/accueil.inc.php";
+}
+include_once "./includes/footer.php";
