e6b5a9dc6578e026c51f8f6d394a3e9cf6ed4c3b
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 11:13:52 +0100

    pdo

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-/*if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
     echo "Vous etes connecte";
 }
 else {
     echo "Vous n'etes pas connecte";
-}*/
-//echo session_id();
+}
+echo session_id();
 date_default_timezone_set('Europe/Paris');
 include_once "./includes/html.php";
 include_once "./includes/header.php";
