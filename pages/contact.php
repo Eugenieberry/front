af68f31d5897865003044dd7e2341fc87c623129
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 15:56:25 +0100

    correction index

--- pages/contact.php
+++ pages/contact.php
@@ -11,13 +11,13 @@
 
     <div><label for="nom"> Nom : </label> <input type="text" id="nom" name="nom" placeholder="saisir votre nom ici" /></div>
 
-    <div><label for="prenom"> prenom : </label> <input type="text" id="prenom" name="prenom" placeholder="saisir votre prenom ici"/></div>
+    <div><label for="prenom"> Prenom : </label> <input type="text" id="prenom" name="prenom" placeholder="saisir votre prenom ici"/></div>
 
-    <div><label for="email"> email : </label> <input type="text" id="email" name="email" placeholder="saisir votre email ici" /></div>
+    <div><label for="email"> Email : </label> <input type="text" id="email" name="email" placeholder="saisir votre email ici" /></div>
 
-    <div><label for="sujet"> mots de passe : </label> <input type="sujet" id="sujet" name="sujet" placeholder="saisir le sujet de votre message ici"/></div>
+    <div><label for="sujet"> Sujet : </label> <input type="sujet" id="sujet" name="sujet" placeholder="saisir le sujet de votre message ici"/></div>
 
-    <div><label for="message"> mots de passe : </label> <input type="message" id="message" name="message" placeholder="saisir votre message ici"/></div>
+    <div><label for="message"> Message : </label> <input type="message" id="message" name="message" placeholder="saisir votre message ici"/></div>
 
 
     <input type="reset" value="effacer">
