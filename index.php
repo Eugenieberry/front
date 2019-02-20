81b3414ae2ccd2993f76b4ae5068f776f641f088
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 14:21:11 +0100

    Creation branch FeatureEB et de la page contact

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
-<?php
-session_start();
-if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
-    echo "Vous etes connecte";
-}
-else {
-    echo "Vous n'etes pas connecte";
-}
-echo session_id();
-date_default_timezone_set('Europe/Paris');
-include_once "./includes/html.php";
-include_once "./includes/header.php";
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-}
-else {
-    $page = "accueil";
-}
-$path = "./includes/";
-$contenu = glob($path . "*.inc.php");
-$page = $path . $page . ".inc.php";
-if (in_array($page, $contenu)) {
-    include_once $page;
-}
-else {
-    include_once "./includes/accueil.inc.php";
-}
-include_once "./includes/footer.php";
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>  formulaire contact</title>
+</head>
+
+<body>
+<h1> Nous contacter</h1>
+<form action="index.html" method="post">
+    <div><label for="nom"> Nom : </label> <input type="text" id="nom" name="nom" placeholder="saisir votre nom ici" /></div>
+
+
+    <div><label for="prenom"> prenom : </label> <input type="text" id="prenom" name="prenom" placeholder="saisir votre prenom ici"/></div>
+
+
+    <div><label for="email"> email : </label> <input type="text" id="email" name="email" placeholder="saisir votre email ici" /></div>
+
+
+    <div><label for="sujet"> mots de passe : </label> <input type="sujet" id="sujet" name="sujet" placeholder="saisir le sujet de votre message ici"/></div>
+
+
+    <div><label for="message"> mots de passe : </label> <input type="message" id="message" name="message" placeholder="saisir votre message ici"/></div>
+
+
+    <input type="reset" value="effacer">
+    <input type="submit" value="Envoyer">
+
+
+</form>
+
+</body>
+</html>
+
+
+
+
