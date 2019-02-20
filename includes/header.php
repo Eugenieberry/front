fe87fc6af9ab540a3cc4f7a9f3b87b25cc120c2e
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-20 16:50:33 +0100

    Essais de modifications sur le menu + style.

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,37 @@
-
 <body>
 <header>
-    <nav>
-        <ul>
-            <li><a href="index.php?page=accueil">Accueil</a></li>
-            <li><a href="index.php?page=produits">Produits</a></li>
-            <li><a href="index.php?page=inscription">Inscription</a></li>
-            <?php
-            if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
-                echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
-            }
-            else {
-                echo "<li><a href=\"index.php?page=login\">Login</a></li>";
-            }
-            ?>
-            <li><a href="index.php?page=contact">Contact</a></li>
-        </ul>
-    </nav>
+    <div id="menu">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="bougies">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Les bougies <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Les boutiques</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        L'univers Kepi
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Notre marque</a>
+                        <a class="dropdown-item" href="#">Nos bougies</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Armada</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button type="button" class="btn btn-danger" type="submit">Rechercher</button>
+            </form>
+        </div>
+        </nav>
+    </div>
 </header>
 <main>
