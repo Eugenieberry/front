6cdc50788e4ee2b6df8e9fdd6801fd9f22318c4a
Eugenie Berry <eugenieberry17@gmail.com>
2019-02-20 10:51:56 +0100

    accueil,html et logout

--- includes/html.php
+++ includes/html.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-    <title><?php echo displayTitle(); ?></title>
     <link href="./assets/css/style.css" rel="stylesheet" />
     <link href="./assets/css/bootstrap.css" rel="stylesheet" />
 </head>
